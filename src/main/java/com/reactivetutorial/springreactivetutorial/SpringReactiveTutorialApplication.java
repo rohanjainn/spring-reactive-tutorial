088a604b838f7d4df37dfa3c670bee8ca8da0ee2
rohanjainn <iamrohanjainn@gmail.com>
2022-12-25 13:25:00 +0530

    initial project setup

---
+++ src/main/java/com/reactivetutorial/springreactivetutorial/SpringReactiveTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.reactivetutorial.springreactivetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivetutorial/springreactivetutorial/SpringReactiveTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivetutorial.springreactivetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
